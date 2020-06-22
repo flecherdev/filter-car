b0649bd2e4ebbbf23b52ba5904741c323da45361
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-06-21 21:37:37 -0300

    add rutes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: 'cars', pathMatch: 'full' },
+      { path: 'cars', loadChildren: () => import('./car/car.module').then( m => m.CarModule ) }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
