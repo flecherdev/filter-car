4e69ddd60eab2b4da12dbf81e1e3e19c7f483b8d
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-06-22 23:25:12 -0300

    added options check

--- src/app/car/components/cars/cars.component.ts
+++ src/app/car/components/cars/cars.component.ts
@@ -1,43 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 import { Car } from '../../../core/model/car';
+import { CarsService } from '../../../core/services/cars/cars.service';
+import { Doc } from '../../../core/model/doc';
 
 @Component({
   selector: 'app-cars',
   templateUrl: './cars.component.html',
-  styleUrls: ['./cars.component.scss']
+  styleUrls: ['./cars.component.scss'],
+  providers: [CarsService]
 })
 export class CarsComponent implements OnInit {
 
-  cars: Car[] = [
-    {
-      title: 'Lifan X70 2.0 Vvt-i Cvt',
-      image: './assets/S3-File-1579186087304-19.jpg',
-      year: '2019',
-      banner: '0km',
-      precio: '1500000',
-      precioOnline: '1500000',
-      moneda: '$',
-      score: '5',
-      caracteristica: 'Nafta transmision manual'
-    },
-    {
-      title: 'Lifan X70 2.0 Vvt-i Cvt',
-      image: './assets/S3-File-1579186087304-19.jpg',
-      year: '2019',
-      banner: '0km',
-      precio: '1500000',
-      precioOnline: '1500000',
-      moneda: '$',
-      score: '5',
-      caracteristica: 'Nafta transmision manual'
-    }
-  ];
+  cars: Car[] = [];
+  data: Doc;
 
-
-
-  constructor() { }
+  constructor(
+    private carsService: CarsService
+  ) { }
 
   ngOnInit() {
+    this.fetchAll();
+  }
+
+  fetchAll() {
+    this.carsService.getAllDoc().subscribe( (doc: [] ) => {
+      this.fechDataCar(doc);
+    });
+  }
+
+  fechDataCar(data: []) {
+
+    data['docs'].map( myCar => {
+      let car: Car = {};
+
+      console.log(myCar['caracteristicas'][5] );
+
+      car.title = myCar['titulo'];
+      car.banner = myCar['banner'];
+      car.caracteristica = `${ myCar['caracteristicas'][5]['caracteristica'] } ${ myCar['caracteristicas'][5]['valor'] }`;
+      car.image = myCar['imagenes'][0]['ruta'];
+      car.moneda = myCar['moneda'];
+      car.precio = myCar['precio'];
+      car.precioOnline = myCar['precioonline'];
+      car.score = myCar['scoring'];
+      car.year = myCar['year'];
+      
+      this.cars.push(car);
+    });
+
   }
 
 }
